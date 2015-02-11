b43fb6bd1c2b7f98e14f7835a4c41a7a2912e60e
Will Fleming <will@mintdigital.com>
2015-02-11 21:03:50 +0000

    method to 'push' loglines into entries

--- punchcard/entry.go
+++ punchcard/entry.go
@@ -1,6 +1,7 @@
 package punchcard
 
 import (
+	"errors"
 	"time"
 )
 
@@ -10,7 +11,35 @@ type Entry struct {
 	project string
 }
 
-func (entry Entry) IsZero() bool {
+func (entry *Entry) IsZero() bool {
 	return (entry.timeIn.IsZero() && entry.timeOut.IsZero() &&
 		"" == entry.project)
+}
+
+// modifies an entry with info in a LogLine (if valid)
+func (entry *Entry) pushLogLine(line LogLine) error {
+	switch {
+	case !entry.timeIn.IsZero() && !entry.timeOut.IsZero():
+		return errors.New("Entry full: no more actions allowed")
+	case entry.timeIn.IsZero() && IN != line.action:
+		return errors.New("Entry must punch in before punching out")
+	case !entry.timeIn.IsZero() && OUT != line.action:
+		return errors.New("Entry must cannot punch in twice")
+	case IN == line.action && "" == line.project:
+		return errors.New("Empty project name not permitted")
+	case OUT == line.action && line.time.Before(entry.timeIn):
+		return errors.New("Punch out time cannot be before in time")
+	}
+
+	switch line.action {
+	case IN:
+		entry.timeIn = line.time
+		entry.project = line.project
+	case OUT:
+		entry.timeOut = line.time
+	default:
+		return errors.New("Invalid LogLine action")
+	}
+
+	return nil
 }
--- punchcard/entry_test.go
+++ punchcard/entry_test.go
@@ -1,6 +1,9 @@
 package punchcard
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestEntryIsZero(t *testing.T) {
 	var entry Entry
@@ -12,4 +15,68 @@ func TestEntryIsZero(t *testing.T) {
 	if entry.IsZero() {
 		t.Error("entry with data should not be zero")
 	}
+}
+
+func TestEntryPushLogLineErrors(t *testing.T) {
+	now := time.Now()
+	var entry Entry
+	if entry.pushLogLine(LogLine{OUT, now, ""}) == nil {
+		t.Error("pushing out to empty entry should error")
+	}
+
+	if entry.pushLogLine(LogLine{IN, now.Add(-500), ""}) == nil {
+		t.Error("pushing empty project should error")
+	}
+
+	entry.project = "foo"
+	entry.timeIn = now.Add(-500)
+
+	if entry.pushLogLine(LogLine{IN, now, "foo"}) == nil {
+		t.Error("punch in twice should error")
+	}
+
+	if entry.pushLogLine(LogLine{-1, now, "foo"}) == nil {
+		t.Error("invalid action should error")
+	}
+
+	if entry.pushLogLine(LogLine{OUT, now.Add(-600), "foo"}) == nil {
+		t.Error("can't punch out before punching in")
+	}
+
+	entry.timeOut = now
+
+	if entry.pushLogLine(LogLine{OUT, now.Add(600), ""}) == nil {
+		t.Error("can't punch out twice")
+	}
+}
+
+func TestEntryPushLogLineSuccess(t *testing.T) {
+	project, now := "foo proj", time.Now()
+	var entry Entry
+
+	if entry.pushLogLine(LogLine{IN, now, project}) != nil {
+		t.Error("punch in should not have failed")
+	}
+
+	if entry.project != project {
+		t.Error("punch in should have set project")
+	}
+
+	if !entry.timeIn.Equal(now) {
+		t.Error("punch in should have set timeIn")
+	}
+
+	future := now.Add(500)
+	if entry.pushLogLine(LogLine{OUT, future, ""}) != nil {
+		t.Error("punch out should not have failed")
+	}
+
+	if entry.project != project {
+		t.Error("punch out should not change project")
+	}
+
+	if !entry.timeOut.Equal(future) {
+		t.Error("punch out should have set timeOut")
+	}
+
 }
