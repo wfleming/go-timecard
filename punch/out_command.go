1789093ea3d23172762f63c852e4d6ba61e817cd
Will Fleming <will@mintdigital.com>
2015-02-12 22:20:54 +0000

    basic out command

--- punch/out_command.go
+++ punch/out_command.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func runOut(config *appConfig, args []string) {
 	if len(args) > 0 && (args[0] == "--help" || args[0] == "-h") {
@@ -8,7 +12,16 @@ func runOut(config *appConfig, args []string) {
 		return
 	}
 
-	fmt.Println("TODO: do punching out")
+	if err := config.log.PunchOut(time.Now()); err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+	lastEntry, err := config.log.LastEntry()
+	if err != nil {
+		fmt.Printf("We wrote to log, but got error getting entry: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Punched out of %s\n", lastEntry.Project)
 }
 
 func printOutHelp() {
