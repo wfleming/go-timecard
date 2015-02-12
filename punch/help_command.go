5d5ceb65599f18c3856a847fc0255a014d4b90d6
Will Fleming <will@mintdigital.com>
2015-02-12 21:07:38 +0000

    bin tool to recognize subcommands & print help

---
+++ punch/help_command.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func runHelp(args []string) {
+	if 0 == len(args) {
+		printMainHelp()
+	} else if c, exists := commands[args[0]]; !exists {
+		printMainHelp()
+	} else {
+		c.printHelp()
+	}
+}
+
+func printMainHelp() {
+	fmt.Println("Punch is a simple time tracker.")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\tpunch <command> [arguments]")
+	fmt.Println("")
+	fmt.Println("The commands are:")
+	fmt.Println("")
+	fmt.Println("\tin\tpunch in to a project")
+	fmt.Println("\tout\tpunch out of a project")
+	fmt.Println("\tsummary\tsummarize your time")
+	fmt.Println("")
+	fmt.Println("Use \"punch help [command]\" for more information about a command.")
+	fmt.Println("")
+}
--- punch/punch.go
+++ punch/punch.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	// "github.com/wfleming/go-punchcard/punchcard"
+	"os"
+)
 
+type command struct {
+	run       func(args []string)
+	printHelp func()
+}
+
+var commands = map[string]command{}
+
+func main() {
+	setupCommands()
+
+	if 0 == len(os.Args[1:]) {
+		commands["help"].run(os.Args[1:])
+		return
+	}
+
+	switch subcommand := os.Args[1]; subcommand {
+	case "in":
+		fmt.Println("TODO punch in")
+	case "out":
+		fmt.Println("TODO to punch out")
+	case "summary":
+		fmt.Println("TODO: to print summary")
+	default:
+		commands["help"].run(os.Args[2:])
+	}
+}
+
+// can't do it as part of variable, or there's a reference loop
+func setupCommands() {
+	commands["help"] = command{runHelp, printMainHelp}
 }
