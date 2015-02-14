bf7c50052b45983e86e81140cd00010113730ebd
Will Fleming <will@mintdigital.com>
2015-02-14 17:48:03 +0000

    CSV output for summary
    this is deeply hacky & inefficient

--- punch/summary_command.go
+++ punch/summary_command.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"github.com/wfleming/go-punchcard/timecard"
 	"os"
+	"sort"
+	"time"
 )
 
-const shortDate = "Monday, 02 January 2006"
+const (
+	csvDate   = "2006-01-02"
+	humanDate = "Monday, 02 January 2006"
+)
 
 func runSummary(config *appConfig, args []string) {
-	if len(args) > 0 {
+	if len(args) > 0 && args[0] != "--csv" {
 		printSummaryHelp()
 		return
 	}
@@ -31,7 +36,11 @@ func runSummary(config *appConfig, args []string) {
 	summary := timecard.NewSummary(entries)
 	daySummaries := summary.GetSummaries()
 
-	printSummariesStd(daySummaries)
+	if len(args) > 0 && args[0] == "--csv" {
+		printSummariesCsv(daySummaries)
+	} else {
+		printSummariesStd(daySummaries)
+	}
 }
 
 func exitIfNoEntries(entries []*timecard.Entry) {
@@ -44,7 +53,7 @@ func exitIfNoEntries(entries []*timecard.Entry) {
 // do the standard printing of the summary data
 func printSummariesStd(summaries []timecard.DaySummary) {
 	for _, daySummary := range summaries {
-		fmt.Println(daySummary.Date.Format(shortDate))
+		fmt.Println(daySummary.Date.Format(humanDate))
 
 		for _, projHours := range daySummary.Hours {
 			fmt.Printf("\t%s: %.2f\n", projHours.Project, projHours.Hours)
@@ -53,6 +62,71 @@ func printSummariesStd(summaries []timecard.DaySummary) {
 	}
 }
 
+// printing the summary data as CSV
+func printSummariesCsv(summaries []timecard.DaySummary) {
+	var allProjects = make([]string, 0)
+
+	indexStr := func(arr []string, needle string) int {
+		for idx, str := range arr {
+			if str == needle {
+				return idx
+			}
+		}
+		return -1
+	}
+
+	hoursForProjDate := func(project string, date time.Time) float64 {
+		for _, daySummary := range summaries {
+			if !daySummary.Date.Equal(date) {
+				continue
+			}
+			for _, projHours := range daySummary.Hours {
+				if projHours.Project == project {
+					return projHours.Hours
+				}
+			}
+		}
+		return 0
+	}
+
+	// iterate once to get all project names
+	for _, daySummary := range summaries {
+		for _, projHours := range daySummary.Hours {
+			if indexStr(allProjects, projHours.Project) < 0 {
+				allProjects = append(allProjects, projHours.Project)
+			}
+		}
+	}
+
+	sort.Strings(allProjects)
+
+	//////// print the CSV data ///////
+	// first line is just dates
+	fmt.Print("Project,")
+	for idx, daySummary := range summaries {
+		var comma = ""
+		if idx < (len(summaries) - 1) {
+			comma = ","
+		}
+		fmt.Printf("%s%s",
+			daySummary.Date.Format(csvDate), comma)
+	}
+	fmt.Print("\n")
+	// now print a row for each project
+	for _, project := range allProjects {
+		fmt.Printf("%s,", project)
+		for idx, daySummary := range summaries {
+			var comma = ""
+			if idx < (len(summaries) - 1) {
+				comma = ","
+			}
+			hours := hoursForProjDate(project, daySummary.Date)
+			fmt.Printf("%.2f%s", hours, comma)
+		}
+		fmt.Print("\n")
+	}
+}
+
 func printSummaryHelp() {
-	fmt.Println("usage: punch summary")
+	fmt.Println("usage: punch summary [--csv]")
 }
