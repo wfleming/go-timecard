2d5d13d95fa5e609088462c9a774d66d166333e8
Will Fleming <will@mintdigital.com>
2015-02-14 12:08:04 +0000

    building summary data structures

--- timecard/summary.go
+++ timecard/summary.go
@@ -16,27 +16,48 @@ type DaySummary struct {
 
 type Summary struct {
 	entries   []Entry
-	Summaries []DaySummary // should be sorted by date
+	summaries []DaySummary // should be sorted by date
 }
 
 func NewSummary(entries []Entry) *Summary {
 	return &Summary{entries, nil}
 }
 
-// builds the summary of the entries
-//TODO: refactor, test
-func (s *Summary) BuildSummary() {
-	// build all the data in a structure of maps: easier while iterating over
-	// a bunch of entries in unknown order. After summarizing, we'll convert
-	// the map into []DaySummary, with appropriate sorting.
-	// the map is date => project name => hours
+func (s *Summary) GetSummaries() []DaySummary {
+	if s.summaries == nil {
+		s.summaries = s.buildSummaries()
+	}
+	return s.summaries
+}
+
+// builds the summary of the entries in s
+func (s *Summary) buildSummaries() []DaySummary {
+	// get map of date => project name => hours
+	//m := s.buildDataMap()
+
+	//TODO transform map to []DaySummary
+	return nil
+}
+
+// build all the data in a structure of maps: easier while iterating over
+// a bunch of entries in unknown order. After summarizing, we'll convert
+// the map into []DaySummary, with appropriate sorting.
+// the map is date => project name => hours
+func (s *Summary) buildDataMap() map[time.Time](map[string]float64) {
 	m := make(map[time.Time](map[string]float64))
 
 	for _, entry := range s.entries {
 		entryDate := atMidnight(entry.TimeIn)
-		projHourMap, _ := m[entryDate]
-		projHourMap[entry.Project] = projHourMap[entry.Project] + entry.Duration().Hours()
+		var projHourMap, exists = m[entryDate]
+		if !exists {
+			m[entryDate] = make(map[string]float64)
+			projHourMap = m[entryDate]
+		}
+		hours := projHourMap[entry.Project] + entry.Duration().Hours()
+		projHourMap[entry.Project] = hours
 	}
+
+	return m
 }
 
 // return a time with all non-date fields set to 0, i.e. represent only the
--- timecard/summary_test.go
+++ timecard/summary_test.go
@@ -34,3 +34,31 @@ func TestAtMidnight(t *testing.T) {
 		t.Error("midnight location does not match original time")
 	}
 }
+
+func TestBuildDataMap(t *testing.T) {
+	d1, _ := time.Parse(time.RFC3339, "2015-02-10T15:00:00Z")
+	a := func(t time.Time, hours float64) time.Time {
+		duration := time.Duration(hours * float64(time.Hour))
+		return t.Add(duration)
+	}
+	entries := []Entry{
+		Entry{d1, a(d1, 1), "task1"},
+		Entry{a(d1, 2), a(d1, 2.5), "task2"},
+		Entry{a(d1, 5), a(d1, 6.5), "task1"},
+		Entry{a(d1, 24), a(d1, 26), "task2"},
+	}
+	s := NewSummary(entries)
+
+	m := s.buildDataMap()
+
+	midnight := atMidnight(d1)
+	if h := m[midnight]["task1"]; h != 2.5 {
+		t.Error("wrong hours for task1 on day 1", h)
+	}
+	if h := m[midnight]["task2"]; h != 0.5 {
+		t.Error("wrong hours for task2 on day 1", h)
+	}
+	if h := m[midnight.Add(24*time.Hour)]["task2"]; h != 2 {
+		t.Error("wrong hours for task2 on day 2", h)
+	}
+}
